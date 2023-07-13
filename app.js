5a92a9b022e957fb28da5419f7c1d55caae30f89
kavins69 <kavinroberts69@gmail.com>
2023-07-13 16:23:15 +0530

    add

--- app.js
+++ app.js
@@ -9,12 +9,13 @@ const logger = require("koa-logger");
 const app = new Koa();
 // app.use(logger());
 // app.use(cors())
-db.sequelize.sync({ force: false});
+console.log("hello")
+db.sequelize.sync({ force: false})
 app.use(koaBody());
 routes.createRoute(app);
 
 
 app.listen(process.env.PORT || PORT, () => {
-    console.log('Server listening in PORT:%s',process.env.PORT || PORT);
+    console.log('Server listening in PORT:%s',process.env.PORT || PORT)
   });
 
